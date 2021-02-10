f4f0a82070d6c87553b2c3b1f43069c0ec8b7b50
SigitaPaulikaite <43108508+syga19@users.noreply.github.com>
2021-02-10 15:23:15 +0200

    Update emailtemplateone.blade.php

--- resources/views/emailtemplateone.blade.php
+++ resources/views/emailtemplateone.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <form action="{{ route('template.update', $post['id']) }}" method="POST">
     @method('PUT') @csrf 
-    <input type="text" name="text" value="{{ $post['text'] }}" class="form-control lg"><br>
+    <textarea id="mce"  name="text" cols="30" rows="4" placeholder="{!! $post['text'] !!}" class="form-control lg"></textarea>
     <input class="btn btn-primary" type="submit" value="UPDATE">
 </form>
 @endsection
